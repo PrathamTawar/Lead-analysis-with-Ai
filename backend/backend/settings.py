b6cd8dec95126d1d9843e31429e4fee62fcb9e72
PrathamTawar <tawpra28@gmail.com>
2025-09-25 14:06:03 +0530

    host allowed render

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'django-insecure-^2z_7705d16cwfm)tkko8ux1uks#$n=-p-pw-6$6qayas@o+@*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["lead-analysis-with-ai.onrender.com"]
 
 
 # Application definition
